522768f987743f42ddfab23e0e12a4e9b87501a3
ragrahari <rupesha157@gmail.com>
2018-12-19 16:42:12 -0500

    deleting unwanted file

--- vars/templatePipeline.groovy
+++
@@ -1,4 +0,0 @@
-@Library('shared-libraries') _
-
-templatePipeline(repoUrl: 'https://github.com/ragrahari/jenkins_shared_lib.git', 
-                 buildName: 'load-lib', imageStreamName: 'load-lib', deploymentName: 'load-lib')
